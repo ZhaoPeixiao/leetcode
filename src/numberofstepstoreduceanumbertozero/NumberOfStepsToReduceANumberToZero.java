efd2441a611b80239921497067623d869c1252bd
Peixiao Zhao <u6935249@anu.edu.au>
2020-07-25 22:44:18 +1000

    Initial commit

---
+++ src/numberofstepstoreduceanumbertozero/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,23 @@
+package numberofstepstoreduceanumbertozero;
+
+/**
+ * @Author: Peixiao Zhao
+ */
+class Solution {
+    public int numberOfSteps (int num) {
+        int steps = 0;
+
+        while(num>0){
+            if(num%2==0){
+                num = num/2;
+                steps++;
+            }
+            if(num%2>0){
+                num = num-1;
+                steps++;
+            }
+        }
+
+        return steps;
+    }
+}
