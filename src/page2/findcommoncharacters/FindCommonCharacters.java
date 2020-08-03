2ff0e916bcb80cc50a19bdb4d15ce35c88e5d2b1
Peixiao Zhao <u6935249@anu.edu.au>
2020-08-03 22:23:12 +1000

    2020.8.3

---
+++ src/page2/findcommoncharacters/FindCommonCharacters.java
@@ -0,0 +1,40 @@
+package page2.findcommoncharacters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: Peixiao Zhao
+ */
+class Solution {
+    public List<String> commonChars(String[] A) {
+        int[] arr = new int[26];
+        int[] temp = new int[26];
+
+        for (char val : A[0].toCharArray()) {
+            arr[val - 'a']++;
+        }
+
+        for (int i = 1; i < A.length; i++) {
+            for (char val : A[i].toCharArray()) {
+                temp[val - 'a']++;
+            }
+
+            for (int j = 0; j < arr.length; j++) {
+                arr[j] = Math.min(arr[j], temp[j]);
+                temp[j] = 0;
+            }
+
+        }
+
+        List<String> ll = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                while (arr[i]-- > 0)
+                    ll.add((char) (i + 'a') + "");
+            }
+        }
+
+        return ll;
+    }
+}
---
+++ src/page2/relativesortarray/RelativeSortArray.java
@@ -0,0 +1,29 @@
+package page2.relativesortarray;
+
+/**
+ * @Author: Peixiao Zhao
+ */
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        if (null == arr1 || arr1.length <= 0 || null == arr2 || arr2.length <= 0) {
+            return arr1;
+        }
+
+        int[] count = new int[1001];
+        for (int n : arr1) count[n]++;
+
+        int i = 0;
+        for (int m : arr2) {
+            while (count[m]-- > 0) {
+                arr1[i++] = m;
+            }
+        }
+
+        for (int j = 0; j < count.length; j++) {
+            while (count[j]-- > 0) {
+                arr1[i++] = j;
+            }
+        }
+        return arr1;
+    }
+}
