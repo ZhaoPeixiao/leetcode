ab8d556d747c64d0dc3777688b2355dc288ac8a9
Peixiao Zhao <u6935249@anu.edu.au>
2020-08-08 19:50:12 +1000

    2020.8.8

---
+++ src/page3/waterbottles/WaterBottles.java
@@ -0,0 +1,16 @@
+package page3.waterbottles;
+
+/**
+ * @Author: Peixiao Zhao
+ */
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int res = numBottles;
+        int left = numBottles;
+        while (left >= numExchange) {
+            res += (left / numExchange);
+            left = (left / numExchange) + (left % numExchange);
+        }
+        return res;
+    }
+}
