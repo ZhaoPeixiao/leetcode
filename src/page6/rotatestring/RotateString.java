af5226a12c50cadea789a81d9cef143584c8d145
Peixiao Zhao <u6935249@anu.edu.au>
2020-08-25 15:53:02 +1000

    2020.8.25

---
+++ src/page6/rotatestring/RotateString.java
@@ -0,0 +1,22 @@
+package page6.rotatestring;
+
+/**
+ * @Author: Peixiao Zhao
+ */
+class Solution {
+    public boolean rotateString(String A, String B) {
+        if (A.equals(B)) {
+            return true;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(A);
+        for (int i = 0; i <A.length(); i ++){
+            stringBuilder.append(stringBuilder.charAt(0));
+            stringBuilder.delete(0, 1);
+            if (stringBuilder.toString().equals(B)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
