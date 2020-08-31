038e36684be463200b71fca5b779bf6e48f1540b
Peixiao Zhao <u6935249@anu.edu.au>
2020-08-31 20:16:59 +1000

    2020.8.31

---
+++ src/page6/guessnumberhigherorlower/GuessNumberHigherorLower.java
@@ -0,0 +1,22 @@
+package page6.guessnumberhigherorlower;
+
+/**
+ * @Author: Peixiao Zhao
+ */
+//public class Solution extends GuessGame {
+//    public int guessNumber(int n) {
+//        int start = 1;
+//        int end = n;
+//        while(start<=end){
+//            int mid = start+(end-start)/2;
+//            if(guess(mid)==0){return mid;}
+//            else if(guess(mid)<0){
+//                end = mid-1;
+//            }
+//            else{
+//                start = mid+1;
+//            }
+//        }
+//        return -1;
+//    }
+//}
---
+++ src/page6/poweroftwo/PowerofTwo.java
@@ -0,0 +1,26 @@
+package page6.poweroftwo;
+
+/**
+ * @Author: Peixiao Zhao
+ */
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        return check(n);
+    }
+
+    public boolean check(int n){
+        if (n == 2){
+            return true;
+        }
+        if (n > 2 && n % 2 == 0){
+            return check(n / 2);
+        }
+        return false;
+    }
+}
