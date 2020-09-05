fef293873d4877c12d44540d1ad7e84f05c15067
Peixiao Zhao <u6935249@anu.edu.au>
2020-09-05 19:58:27 +1000

    2020.9.5

---
+++ src/page7/numberofsegmentsinastring/NumberofSegmentsinaString.java
@@ -0,0 +1,15 @@
+package page7.numberofsegmentsinastring;
+
+/**
+ * @Author: Peixiao Zhao
+ */
+class Solution {
+    public int countSegments(String s) {
+        s = s.trim();
+        if (s.isEmpty()) {
+            return 0;
+        }
+        String[] ans = s.split("\\s+");
+        return ans.length;
+    }
+}
