6774f8b151953e486890ceca886d1e178b5d5baf
Peixiao Zhao <u6935249@anu.edu.au>
2020-09-09 19:32:37 +1000

    2020.9.9

---
+++ src/page8/validmountainarray/ValidMountainArray.java
@@ -0,0 +1,27 @@
+package page8.validmountainarray;
+
+/**
+ * @author Peixiao Zhao
+ * @date 2020/9/9
+ */
+class Solution {
+    public boolean validMountainArray(int[] A) {
+        int l = A.length, descend = 0, ascend = 0;
+        if (l < 3)
+            return false;
+        for (int i = 1; i < l; i++) {
+            if (A[i] > A[i - 1] && descend == 0)
+                ascend++;
+            else if (A[i] < A[i - 1] && ascend > 0) {
+                descend++;
+            }
+            else {
+                return false;
+            }
+        }
+        if (descend > 0 && ascend > 0)
+            return true;
+        else
+            return false;
+    }
+}
