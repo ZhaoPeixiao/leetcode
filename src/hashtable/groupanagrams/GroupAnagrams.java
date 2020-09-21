363104fc84914bcd8f1ba5e0a9ff8c6d97382445
Peixiao Zhao <u6935249@anu.edu.au>
2020-09-21 22:19:16 +1000

    2020.9.21 feel unwell...

---
+++ src/hashtable/groupanagrams/GroupAnagrams.java
@@ -0,0 +1,21 @@
+package hashtable.groupanagrams;
+
+import java.util.*;
+
+/**
+ * @author Peixiao Zhao
+ * @date 2020/9/21 20:54
+ */
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs){
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
